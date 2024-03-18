11d3eaeb36104276b3abd23a6241d05caec85d79
omar <el.ell.elhassani@gmail.com>
2024-03-17 16:53:47 -1100

    clerk auth success

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <LeftSidebar />
+          <section className="main-container">
+            <div className="w-full max-w-4xl">{children}</div>
+          </section>
+          <RightSidebar />
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
